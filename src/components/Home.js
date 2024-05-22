af10cd00ace9fb655c8c37c97bf35330c920f1fb
Ali ahmet Erdoğdu <aliahmeterdogdu@192.168.1.109>
2024-05-22 04:08:15 +0300

    Set up GitHub deployment truely

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import { Container } from '../styles';
 
 const Home = () => (
   <section>
-    <h1>Welcome to My Portfolio</h1>
+    <h1>Welcome tsso My Portfolio</h1>
     <h2>Hello, I am Ali Ahmet Erdoğdu.I am a junior student who is interested in mobile development, and I keep improving myself in various fields.</h2>
     <p>I am a junior student who is interested in mobile development, and I keep improving myself in various fields.</p>
   </section>
