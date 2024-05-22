a013d4f6ec71f0deb815421baad1e9246f2174f2
Ali ahmet Erdoğdu <aliahmeterdogdu@192.168.1.109>
2024-05-22 05:06:46 +0300

    deneme1/deneme2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,34 @@
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Header from './components/Header';
+// import Home from './components/Home';
+// import About from './components/About';
+// import Projects from './components/Projects';
+// import Skills from './components/Skills';
+// import Experience from './components/Experience';
+// import Education from './components/Education';
+// import Contact from './components/Contact';
+
+// function App() {
+//   return (
+//     <Router>
+//       <Header />
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/about" element={<About />} />
+//         <Route path="/projects" element={<Projects />} />
+//         <Route path="/skills" element={<Skills />} />
+//         <Route path="/experience" element={<Experience />} />
+//         <Route path="/education" element={<Education />} />
+//         <Route path="/contact" element={<Contact />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
 import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
@@ -11,18 +39,15 @@ import Contact from './components/Contact';
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/experience" element={<Experience />} />
-        <Route path="/education" element={<Education />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>
+    <div>
+      <div id="home"><Home /></div>
+      <div id="about"><About /></div>
+      <div id="projects"><Projects /></div>
+      <div id="skills"><Skills /></div>
+      <div id="experience"><Experience /></div>
+      <div id="education"><Education /></div>
+      <div id="contact"><Contact /></div>
+    </div>
   );
 }
 
