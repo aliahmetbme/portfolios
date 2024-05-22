f722b8d74dd8970e4f05db428a8b8b67f238a29c
Ali ahmet Erdoğdu <aliahmeterdogdu@192.168.1.109>
2024-05-22 03:42:35 +0300

    aa

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import { Container } from '../styles';
+
+const Home = () => (
+  <section>
+    <h1>Welcome to My Portfolio</h1>
+    <h2>Hello, I am Ali Ahmet Erdoğdu.I am a junior student who is interested in mobile development, and I keep improving myself in various fields.</h2>
+    <p>I am a junior student who is interested in mobile development, and I keep improving myself in various fields.</p>
+  </section>
+);
+
+export default Home;
+// Apply styles in your components
+
+
